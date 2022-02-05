cbead645b961e594493997c184f69da863c4f44f
Abhinav <abhinav.zimmy@gmail.com>
2022-02-06 03:09:39 +0530

    created basic app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+const axios = require("axios");
 
 function App() {
+  const [lat, setLat] = useState("");
+  const [lon, setLon] = useState("");
+
+  const API_Key = "bf7f75d820cea9ae68d1292fc1c1c8e4";
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_Key}`;
+
+  const setCurrentLoc = (e) => {
+    e.preventDefault();
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+      console.log(position);
+    });
+    console.log(lon);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .get(url)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => alert(err));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input
+          type="text"
+          value={lat}
+          onChange={(e) => setLat(e.target.value)}
+          placeholder="latitude"
+        ></input>
+        <input
+          type="text"
+          value={lon}
+          onChange={(e) => setLon(e.target.value)}
+          placeholder="longitude"
+        ></input>
+        <button onClick={setCurrentLoc}>get current loc</button>
+        <button type="submit" onClick={onSubmit}>
+          {" "}
+          submit{" "}
+        </button>
+      </form>
     </div>
   );
 }
